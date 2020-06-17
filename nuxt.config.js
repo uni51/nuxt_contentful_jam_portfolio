fdfb43c7bb55bb6e69985a672628b9f8f2b9fa09
uni51 <uni51@users.noreply.github.com>
2020-06-17 09:31:23 +0900

    2-1 Nuxt.jsアプリを新規作成しよう

---
+++ nuxt.config.js
@@ -0,0 +1,70 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@fortawesome/fontawesome-svg-core/styles.css'  
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '~plugins/font-awesome', ssr: false }   
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/markdownit',
+    'nuxt-fontawesome'  
+  ],
+
+  markdownit: { 
+    html: true,
+    injected: true,
+    linkify: true,
+    breaks: false
+  },
+
+  fontawesome: {
+    component: 'fa'
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
